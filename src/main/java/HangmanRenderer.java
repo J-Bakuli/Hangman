e86a58888748d06cf04816d931f95b9c52ecc47e
J-Bakuli <julia.bakuli@yandex.ru>
2025-10-08 14:51:15 +0300

    Refactor HangmanRenderer to use staged representation

--- src/main/java/HangmanRenderer.java
+++ src/main/java/HangmanRenderer.java
@@ -1,26 +1,88 @@
 package main.java;
 
 public final class HangmanRenderer {
+    private static final String[] HANGMAN_STAGES = {
+            " =========" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |______",
+
+            " =========" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       O" + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |______",
+
+            " =========" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       O" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |______",
+
+            " =========" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       O" + System.lineSeparator() +
+                    " |      /|" + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |______",
+
+            " =========" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       O" + System.lineSeparator() +
+                    " |      /|\\" + System.lineSeparator() +
+                    " |       " + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |______",
+
+            " =========" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       O" + System.lineSeparator() +
+                    " |      /|\\" + System.lineSeparator() +
+                    " |      /" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |______",
+
+            " =========" + System.lineSeparator() +
+                    " |       |" + System.lineSeparator() +
+                    " |       O" + System.lineSeparator() +
+                    " |      /|\\" + System.lineSeparator() +
+                    " |      / \\" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |" + System.lineSeparator() +
+                    " |______"
+    };
+
+    private static final int MAX_ERRORS = HANGMAN_STAGES.length - 1;
+
     private HangmanRenderer() {
+    }
+
+    public static String getStage(int errorCount) {
+        if (errorCount < 0) {
+            throw new IllegalArgumentException("Количество ошибок не может быть отрицательным");
+        }
+        return HANGMAN_STAGES[Math.min(errorCount, MAX_ERRORS)];
     }
 
     public static void display(int errorCount) {
         render(errorCount);
     }
 
-    private static void render(int errorCount) {
-        System.out.println(" =========");
-        System.out.println(" |       |");
-        System.out.println(" |       " + (errorCount > 0 ? "O" : ""));
-        System.out.println(" |      " +
-                (errorCount > 1 ? "/" : "") +
-                (errorCount > 2 ? "|" : "") +
-                (errorCount > 3 ? "\\" : ""));
-        System.out.println(" |      " +
-                (errorCount > 4 ? "/" : "") +
-                (errorCount > 5 ? " \\" : ""));
-        System.out.println(" |");
-        System.out.println(" |");
-        System.out.println(" |______");
+    public static void render(int count) {
+        System.out.println(getStage(count));
     }
 }
